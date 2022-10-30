1b51cecd18faaf4dcad2d902cf1fd522deedd64f
gerdriese <61990543+gerdriese@users.noreply.github.com>
2022-10-30 19:31:07 +0100

    Update auf Python 3.11

--- install_all.cmd
+++ install_all.cmd
@@ -1,11 +1,11 @@
 @echo off
 echo Installation von 7Zip, Windows Terminal, VSCode und Python sowie einigen Plugins fuer Python.
 
-for %%G in (7zip.7zip Microsoft.WindowsTerminal Microsoft.VisualStudioCode Python.Python.3) do (
+for %%G in (7zip.7zip Microsoft.WindowsTerminal Microsoft.VisualStudioCode Python.Python.3.11) do (
     winget install %%G
 )
 
-@IF EXIST "%LOCALAPPDATA%\Programs\Python\Python310\python.exe" "%LOCALAPPDATA%\Programs\Python\Python310\python.exe" -m pip install --upgrade pip
+@IF EXIST "%LOCALAPPDATA%\Programs\Python\Python310\python.exe" "%LOCALAPPDATA%\Programs\Python\Python311\python.exe" -m pip install --upgrade pip
 
 for %%G in (ms-python.python ms-python.vscode-pylance ms-toolsai.jupyter njpwerner.autodocstring mechatroner.rainbow-csv janisdd.vscode-edit-csv) do (
 	@echo Installing %%G
