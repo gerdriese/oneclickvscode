b49a1072b6e5484820c160859a4135d1ed38afa7
gerdriese <61990543+gerdriese@users.noreply.github.com>
2022-10-30 19:36:04 +0100

    Update install_all.cmd

--- install_all.cmd
+++ install_all.cmd
@@ -1,13 +1,13 @@
 @echo off
 echo Installation von 7Zip, Windows Terminal, VSCode und Python sowie einigen Plugins fuer Python.
 
-for %%G in (7zip.7zip Microsoft.WindowsTerminal Microsoft.VisualStudioCode Python.Python.3.11) do (
+for %%G in (Microsoft.VisualStudioCode Python.Python.3.11) do (
     winget install %%G
 )
 
 @IF EXIST "%LOCALAPPDATA%\Programs\Python\Python310\python.exe" "%LOCALAPPDATA%\Programs\Python\Python311\python.exe" -m pip install --upgrade pip
 
-for %%G in (ms-python.python ms-python.vscode-pylance ms-toolsai.jupyter njpwerner.autodocstring mechatroner.rainbow-csv janisdd.vscode-edit-csv) do (
+for %%G in (ms-python.python njpwerner.autodocstring mechatroner.rainbow-csv janisdd.vscode-edit-csv) do (
 	@echo Installing %%G
     @"%LOCALAPPDATA%\Programs\Microsoft VS Code\bin\code" --install-extension  %%G > NUL
 )
