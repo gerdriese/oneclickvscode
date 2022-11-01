54a029f1c6b3633f1d3aabf5d378d346711a7b1c
gerdriese <61990543+gerdriese@users.noreply.github.com>
2022-11-01 20:31:01 +0100

    Anpassung Echo, Update PIP auf 3.11 angepasst

--- install_all.cmd
+++ install_all.cmd
@@ -1,11 +1,11 @@
 @echo off
-echo Installation von 7Zip, Windows Terminal, VSCode und Python sowie einigen Plugins fuer Python.
+echo Installation von VSCode und Python sowie einigen Plugins fuer Python.
 
 for %%G in (Microsoft.VisualStudioCode Python.Python.3.11) do (
     winget install %%G
 )
 
-@IF EXIST "%LOCALAPPDATA%\Programs\Python\Python310\python.exe" "%LOCALAPPDATA%\Programs\Python\Python311\python.exe" -m pip install --upgrade pip
+@IF EXIST "%LOCALAPPDATA%\Programs\Python\Python311\python.exe" "%LOCALAPPDATA%\Programs\Python\Python311\python.exe" -m pip install --upgrade pip
 
 for %%G in (ms-python.python njpwerner.autodocstring mechatroner.rainbow-csv janisdd.vscode-edit-csv) do (
 	@echo Installing %%G
